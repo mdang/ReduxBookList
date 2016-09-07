5607e775a87058adb8f83c2a6fce4d1ab33985f3
Mike Dang <mike.dang@ga.co>
2016-09-06 19:21:18 -0500

    Finished tutorial

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'JavaScript: The Good Parts', pages: 101 },
+    { title: 'Book Two', pages: 23 },
+    { title: 'Book Three', pages: 345 },
+    { title: 'Book Four', pages: 77 }
+  ]
+}
